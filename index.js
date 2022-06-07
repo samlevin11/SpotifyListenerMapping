a7c9a309a9a94871315e391f39c2b0796b4248da
Sam Levin <samlevin11@gmail.com>
2022-06-06 20:57:07 -0600

    Starting Spotify authorization process

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const querystring = require('querystring');
+const app = express();
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.get('/login', (req, res) => {
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI,
+    });
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
+});
