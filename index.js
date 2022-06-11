3ab0bc7ffcaf5d9dd2f8ea84f0fbf588beb76ee3
Sam Levin <samlevin11@gmail.com>
2022-06-11 13:11:59 -0600

    Express path logger added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const { nextTick } = require('process');
 const querystring = require('querystring');
 const app = express();
-const port = 8888;
+const port = 3000;
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+    res.send('root');
+});
 
 app.get('/login', (req, res) => {
     const queryParams = querystring.stringify({
@@ -16,6 +23,11 @@ app.get('/login', (req, res) => {
     });
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
+
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
 
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
