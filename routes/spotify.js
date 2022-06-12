344b82a8b354aaaa5772dbdaea3e5dc94eea2538
Sam Levin <samlevin11@gmail.com>
2022-06-12 14:03:14 -0600

    getFollowedArtist function refined
    Better handling for recursive axios request using Promises
    Full list of followed artists can now be accessed as resolved response
    to getFollowedArtists
    Uses closures to keep variables in scope of function

--- routes/spotify.js
+++ routes/spotify.js
@@ -5,13 +5,13 @@ const { json } = require('express/lib/response');
 
 router.get('/', (req, res) => {
     // res.render('map');
-
-    followedArtists = getFollowedArtists(req.query.access_token, res);
-    console.log('FOLLOWED ARTISTS');
-    console.log(followedArtists);
+    getFollowedArtists(req.query.access_token).then((response) => {
+        console.log(response.length);
+        res.json(response);
+    });
 });
 
-function getFollowedArtists(access_token, res) {
+function getFollowedArtists(access_token) {
     console.log('Getting followed artists');
 
     const token = access_token;
@@ -22,37 +22,42 @@ function getFollowedArtists(access_token, res) {
     return followNextArtists(initialNext, [], token);
 
     function followNextArtists(next, followedArtists, access_token) {
-        axios({
-            method: 'get',
-            url: next,
-            headers: {
-                Authorization: `Bearer ${access_token}`,
-            },
-        })
-            .then((response) => {
-                followedArtists = followedArtists.concat(
-                    response.data.artists.items
-                );
-                console.log(
-                    `Collected ${followedArtists.length} of ${response.data.artists.total} artists`
-                );
-
-                console.log(response.data.artists.next);
-                if (response.data.artists.next) {
-                    followNextArtists(
-                        response.data.artists.next,
-                        followedArtists,
-                        access_token
-                    );
-                } else {
-                    console.log('No more artists!');
-                    res.json(followedArtists)
-                    return followedArtists;
-                }
+        return new Promise((resolve, reject) => {
+            axios({
+                method: 'get',
+                url: next,
+                headers: {
+                    Authorization: `Bearer ${access_token}`,
+                },
             })
-            .catch((error) => {
-                console.log(error);
-            });
+                .then((response) => {
+                    followedArtists = followedArtists.concat(
+                        response.data.artists.items
+                    );
+                    console.log(
+                        `Collected ${followedArtists.length} of ${response.data.artists.total} artists`
+                    );
+
+                    if (response.data.artists.next) {
+                        console.log(
+                            `More artists, ${response.data.artists.next}`
+                        );
+                        resolve(
+                            followNextArtists(
+                                response.data.artists.next,
+                                followedArtists,
+                                access_token
+                            )
+                        );
+                    } else {
+                        console.log('No more artists!');
+                        resolve(followedArtists);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        });
     }
 }
 
