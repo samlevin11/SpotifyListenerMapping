a81dbbd429d9ba6bffaefb2301e3d11249d5ac9f
Sam Levin <samlevin11@gmail.com>
2022-06-20 19:54:07 -0600

    Simple function to convert to GeoJSON

---
+++ cityToGeoJSON.js
@@ -0,0 +1,18 @@
+function cityToGeoJSON(properties, coordinates) {
+    return {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates,
+        },
+        properties,
+    };
+}
+
+let coords = [151.21, -33.868];
+let attribs = {
+    city: 'Sydney, AU',
+    listeners: 99999
+}
+
+console.log(cityToGeoJSON(attribs, coords));
