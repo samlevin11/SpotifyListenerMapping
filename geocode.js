904bed8d111639d34d344c9d0504731a7014f2db
samlevin11 <samlevin11@gmail.com>
2022-07-04 16:44:55 -0600

    Geocode simplified with async/await
    Returns coordinates directly
    Simpler assignment to the a top city

--- geocode.js
+++ geocode.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 mapbox_token = process.env.MAPBOX_TOKEN;
 
-function geocodeCity(city) {
+async function geocodeCity(city) {
     const cityFormatted = city.split(', ').join('%20');
 
     var config = {
@@ -12,26 +12,14 @@ function geocodeCity(city) {
         headers: {},
     };
 
-    return axios(config);
+    const geocodeResult = await axios(config);
+    return geocodeResult.data.features[0].center;
 }
 
 module.exports = geocodeCity;
 
-// geocodeCity('Chicago, US').then((response) => {
-//     if (response.status === 200) {
-//         // console.log('response data', response.data);
-//         console.log(response.data.features[0].center)
-//     } else {
-//         console.log('Geocode Error');
-//     }
-// });
-
 // const cities = ['Dallas, US', 'London, UK', 'Sydney, AU', 'Berlin, DE', 'Mexico City, MX']
 // let cityGeocodes = cities.map((city) => geocodeCity(city))
 // Promise.all(cityGeocodes).then((results) => {
-//     let geocodes = results.map((result) => {
-//         return {query: result.data.query,
-//         coords: result.data.features[0].center}
-//     })
-//     console.log(geocodes)
+//     console.log(results)
 // })
--- scrapeGeocode.js
+++ scrapeGeocode.js
@@ -4,19 +4,19 @@ const geocode = require('./geocode');
 async function getArtistStats(artistUrl) {
     console.log(artistUrl);
 
-    let artistStats = await scrape(artistUrl);
+    // let artistStats = await scrape(artistUrl);
     // Dummy artistStats for testing
-    // let artistStats = {
-    //     Followers: 240949,
-    //     MonthlyListeners: 355397,
-    //     topCities: [
-    //         { city: 'London, GB', listeners: 5702 },
-    //         { city: 'Los Angeles, US', listeners: 5626 },
-    //         { city: 'Melbourne, AU', listeners: 5146 },
-    //         { city: 'Istanbul, TR', listeners: 4549 },
-    //         { city: 'Sydney, AU', listeners: 4431 },
-    //     ],
-    // };
+    let artistStats = {
+        Followers: 240949,
+        MonthlyListeners: 355397,
+        topCities: [
+            { city: 'London, GB', listeners: 5702 },
+            { city: 'Los Angeles, US', listeners: 5626 },
+            { city: 'Melbourne, AU', listeners: 5146 },
+            { city: 'Istanbul, TR', listeners: 4549 },
+            { city: 'Sydney, AU', listeners: 4431 },
+        ],
+    };
     // console.log(artistStats);
 
     // Geocode top cities
@@ -25,10 +25,8 @@ async function getArtistStats(artistUrl) {
     artistStats.topCities = await Promise.all(
         artistStats.topCities.map(async (c) => {
             // Geocode via Mapbox geocoding service
-            // Wait for result
-            let cityGeocode = await geocode(c.city);
             // Assign a new coordinates property to the city object with the geocoded long, lat
-            c.coordinates = cityGeocode.data.features[0].center;
+            c.coordinates = await geocode(c.city);
             // return the new city object with coodinate attribute
             return c;
         })
