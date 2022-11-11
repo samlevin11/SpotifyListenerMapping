87718a2d1ab969f495fbefa4fd43563ea4b3a12b
samlevin11 <samlevin11@gmail.com>
2022-11-11 10:37:02 -0700

    Progress on GeoJSON format
    Passing as string, then parsing back to object in map.
    Should pass as object, no need to convert to string

--- routes/spotify.js
+++ routes/spotify.js
@@ -70,21 +70,26 @@ router.get('/mapartist', (req, res) => {
     console.log(req.query);
     let artistUrl = 'https://open.spotify.com/artist/' + req.query.artist;
     getArtistStats(artistUrl).then((artistStats) => {
-        console.log(JSON.stringify(artistStats.topCities));
+        // console.log(JSON.stringify(artistStats.topCities));
         // res.json(artistStats);
-        res.render('map.ejs', { topCities: artistStats.topCities });
+        res.render('map.ejs', {
+            topCities: JSON.stringify(artistStats.topCities),
+        });
     });
 });
 
 router.get('/maptest', (req, res) => {
     console.log('MAP TEST');
 
-    const dummystats = JSON.parse(
-        '{ "Followers": 38661243, "MonthlyListeners": 51021897, "topCities": { "type": "FeatureCollection", "features": [ { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-0.12764739999999997, 51.507321899999994] }, "properties": { "city": "London, GB", "listeners": 1126607 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-118.242766, 34.053691] }, "properties": { "city": "Los Angeles, US", "listeners": 836232 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [151.21, -33.868] }, "properties": { "city": "Sydney, AU", "listeners": 803698 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-87.624421, 41.875562] }, "properties": { "city": "Chicago, US", "listeners": 744136 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-96.80433099999999, 32.864527] }, "properties": { "city": "Dallas, US", "listeners": 644563 } } ] } }'
+    // const dummystats = JSON.parse(
+    //     '{ "Followers": 38661243, "MonthlyListeners": 51021897, "topCities": { "type": "FeatureCollection", "features": [ { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-0.12764739999999997, 51.507321899999994] }, "properties": { "city": "London, GB", "listeners": 1126607 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-118.242766, 34.053691] }, "properties": { "city": "Los Angeles, US", "listeners": 836232 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [151.21, -33.868] }, "properties": { "city": "Sydney, AU", "listeners": 803698 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-87.624421, 41.875562] }, "properties": { "city": "Chicago, US", "listeners": 744136 } }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [-96.80433099999999, 32.864527] }, "properties": { "city": "Dallas, US", "listeners": 644563 } } ] } }'
+    // );
+    const dummystats2 = JSON.parse(
+        '{"Followers":38661243,"MonthlyListeners":50952635,"topCities":{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[-0.12764739999999997,51.507321899999994]},"properties":{"city":"London, GB","listeners":1125142}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-118.242766,34.053691]},"properties":{"city":"Los Angeles, US","listeners":843605}},{"type":"Feature","geometry":{"type":"Point","coordinates":[151.21,-33.868]},"properties":{"city":"Sydney, AU","listeners":801436}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-87.624421,41.875562]},"properties":{"city":"Chicago, US","listeners":741454}},{"type":"Feature","geometry":{"type":"Point","coordinates":[-96.80433099999999,32.864527]},"properties":{"city":"Dallas, US","listeners":642921}}]}}'
     );
 
     res.render('map.ejs', {
-        topCities: JSON.stringify(dummystats.topCities),
+        topCities: JSON.stringify(dummystats2.topCities),
         // topCities: dummystats.topCities
     });
 });
